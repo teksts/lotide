91a235e7564c5419680e2a7b6e7e64188615c4b2
teksts <mark.egelhoff1@gmail.com>
2023-05-29 14:56:10 -0700

    Improved function by refactoring output using template literals.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion passed: [" + actual + "] === [" + expected + "]");
+    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
   } else {
-    console.log("🛑 Assertion failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
   }
 };
 
