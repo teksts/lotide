501836b98a18917861ad4d317b9ccd5109210f59
teksts <mark.egelhoff1@gmail.com>
2023-05-30 14:46:03 -0700

    Implemented a flatten function for converting nested arrays into 1-dimensional arrays.

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    assertEqual(actual[i], expected[i]);
+  }
+};
+
+const flatten = function(arr) {
+  let output = [];
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      for (let subelement of element) {
+        output.push(subelement);
+      }
+    } else {
+      output.push(element);
+    }
+  }
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4]]));
