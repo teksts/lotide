92768777b483a1039c5a88335bc191d22967b8c1
teksts <mark.egelhoff1@gmail.com>
2023-05-31 14:22:43 -0700

    Implemented a function for finding a key that contains a particular value within an object.

---
+++ findKeyByValue.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    assertEqual(actual[i], expected[i]);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const findKeyByValue = function(obj, value) {
+  for (let i in obj) {
+    if (obj[i] === value) {
+      return i;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  mystery: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
