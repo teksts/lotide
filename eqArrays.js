a8d115c68ff10b4ca0634d915695bd36ed54ee9a
teksts <mark.egelhoff1@gmail.com>
2023-06-05 12:11:39 -0700

    Refactored eqArrays to export its function, and separated tests into a distinct file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -21,15 +13,6 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
-console.log("Nested test 1:", eqArrays([1, 2, [3]], [1, 2, [3]])); // => true
-console.log("Nested test 2:", eqArrays([1, 2, 3], [1, 2, [3]])); // => false
-console.log("Nested test 3:", eqArrays([1, [2, 3]], [1, [2, 3]])); // => true
-console.log("Nested test 4:", eqArrays([1, 2, [3]], [1, 2, 3])); // => false
-console.log("Nested test 5:", eqArrays([1, [[2, 3]]], [1, [[2, 3]]])); // => true
-console.log("Nested test 6:", eqArrays([1, [[2, [3]]]], [1, [[2, 3]]])); // => false
+// Export code
+module.exports = eqArrays;
