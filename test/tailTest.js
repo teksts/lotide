b4dda9df272da6d216982821035e5627542814d5
teksts <mark.egelhoff1@gmail.com>
2023-06-05 11:12:29 -0700

    Created a test file for tail.js

---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[1], tail(words)[0]);
