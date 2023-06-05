daa5a7bb3d4b1d3e36f21269f83778775063a20d
teksts <mark.egelhoff1@gmail.com>
2023-06-05 13:10:14 -0700

    Refactored testing infrastructure to employ Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+describe("#head", () => {
+  it('returns Hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+describe("#head", () => {
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
 
 // TESTS
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+
+
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
