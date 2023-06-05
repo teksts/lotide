acacd7839cd9edd3b05a2f9d0051f73df46c4272
teksts <mark.egelhoff1@gmail.com>
2023-06-05 12:51:31 -0700

    Refactored assertArraysEqual to export its function, and separated tests into a distinct file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
-  }
-};
+const eqArrays = require('./eqArrays');
+
 
 const assertArraysEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (!eqArrays(actual, expected)) {
-      console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
-      break;
-    }
+  if (!eqArrays(actual, expected)) {
+    console.log(`🛑 Assertion failed: [${actual}] !== [${expected}]`);
+  } else {
+    console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
   }
-  console.log(`✅ Assertion passed: [${actual}] === [${expected}]`);
 };
 
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// Export code
+module.exports = assertArraysEqual;
